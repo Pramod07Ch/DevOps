951fd3a2ae0a6b76c1b50a01aef97c316ef5c9a9
Pramod kumar chinthala <50557615+Pramod07Ch@users.noreply.github.com>
2023-03-26 13:01:46 +0200

    Add files via upload

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
-echo "I am mysql"
+# set -e
+
+# validating existing user is a root user or not
+ID=$(id -u) # u gives particular userid
+
+COMPONENT=mysql
+
+source components/common.sh
+
+echo -n "Configuring the $COMPONENT repo :"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo 
+check_status $?
+
+echo -n "Installing the $COMPONENT :"
+yum install mysql-community-server -y  &>> $LOGFILE  
+check_status $? 
+
+echo -n "Starting $COMPONENT :" 
+systemctl enable mysqld  &>> $LOGFILE  
+systemctl start mysqld   &>> $LOGFILE  
+check_status $?
