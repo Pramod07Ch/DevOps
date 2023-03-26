124ef3e30e32f7222b992f4fdafbbe65bc118e9e
Pramod kumar chinthala <50557615+Pramod07Ch@users.noreply.github.com>
2023-03-26 07:21:41 +0200

    Add files via upload

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -52,7 +52,7 @@ echo -n "Starting the service:"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log
 
-f [$? -eq 0] ; then
+f [ $? -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
 else
     echo -e "\e[31m Failure \e[0m"
