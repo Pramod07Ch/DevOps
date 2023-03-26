6171a3855b71bcd0f3f30269b0abb329b1ef7513
Pramod kumar chinthala <50557615+Pramod07Ch@users.noreply.github.com>
2023-03-26 07:34:07 +0200

    Add files via upload

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,9 +2,21 @@
 
 # set -e
 
-
 # validating existing user is a root user or not
 ID=$(id -u) # u gives particular userid
+
+COMPONENT=frontend
+LOG_FILE = "/tmp/$COMPONENT.log"
+
+# functions
+check_status() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m Failure \e[0m"
+        exit 2
+    fi
+}
 
 if [ "$ID" -ne 0 ] ; then 
     echo -e "\e[31m You should execute the script as a root user or sudo as a prefix \e[0m"
@@ -12,49 +24,29 @@ if [ "$ID" -ne 0 ] ; then
 fi
 
 echo -n "Installing nginx:"
-yum install nginx -y &>> /tmp/frontend.log
+yum install nginx -y &>> $LOG_FILE
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi
+check_status $?
 
-echo -n "Downloading frontend:"
+echo -n "Downloading $COMPONENT:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi
+check_status $?
 
-echo -n "Performaing cleanup of old frontend content:"
+echo -n "Performaing cleanup of old $COMPONENT content:"
 cd /usr/share/nginx/html
-rm -rf * &>> /tmp/frontend.log
+rm -rf * &>> $LOG_FILE
 
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+unzip /tmp/$COMPONENT.zip &>> $LOG_FILE
 mv frontend-main/* .
 mv static/* .
-rm -rf frontend-main README.md 
+rm -rf $COMPONENT-main README.md 
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi
+check_status $?
 
 echo -n "Starting the service:"
-systemctl enable nginx &>> /tmp/frontend.log
-systemctl start nginx &>> /tmp/frontend.log
+systemctl enable nginx &>> $LOG_FILE
+systemctl start nginx &>> $LOG_FILE
 
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi
+check_status $?
