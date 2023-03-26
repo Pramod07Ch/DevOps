3e2318504659240497bc86d9c3b7e3618dad27d1
Pramod kumar chinthala <50557615+Pramod07Ch@users.noreply.github.com>
2023-03-26 12:51:11 +0200

    Add files via upload

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -2,16 +2,13 @@
 
 # set -e
 
-# validating existing user is a root user or not
-ID=$(id -u) # u gives particular userid
-
 COMPONENT=rabbitmq
 
 source components/common.sh # source loads the file
 
 echo -n "Installing and configuring dependency:"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash   &>> $LOGFILE 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>> $LOGFILE 
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash   &>> $LOG_FILE 
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>> $LOG_FILE 
 check_status $?
 
 echo -n "Installing server:"
@@ -25,15 +22,15 @@ check_status $?
 
 
 
-rabbitmqctl list_users | grep $APPSUSER  &>> $LOGFILE 
+rabbitmqctl list_users | grep $APPSUSER  &>> $LOG_FILE 
 if [ $? -ne 0 ] ; then
     echo -n "creating $COMPONENT applicaton user"
-    rabbitmqctl add_user roboshop roboshop123   &>> $LOGFILE 
+    rabbitmqctl add_user roboshop roboshop123   &>> $LOG_FILE 
     check_status $?
 
 fi
 
 echo -n "Adding required privileges to the $APPUSER :"
-    rabbitmqctl set_user_tags roboshop administrator           &>> $LOGFILE 
-    rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"   &>> $LOGFILE 
+    rabbitmqctl set_user_tags roboshop administrator           &>> $LOG_FILE 
+    rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"   &>> $LOG_FILE 
     check_status $?
