5abaf31772531bf98a15082e5879ed1edf58ccf3
Pramod kumar chinthala <pramodkumar.chinthala@gmail.com>
2023-04-14 20:25:10 +0200

    Auto push terraform

--- terraform_basics/02-vars/sample.tf
+++ terraform_basics/02-vars/sample.tf
@@ -12,3 +12,27 @@ output "sample_op_2" {
 
 # variable can be accessed without ${}
 # if given ina string then its required
+
+variable "integer" {
+    default     =   1000
+}
+
+output "number" {
+    value   =   var.number
+}
+
+# list
+variable "sample_list" {
+    default     =   [
+                "Terraform",
+                "Training",
+                99,
+                true,
+                "Cloud formation"
+    ]
+}
+
+# from the above list bock, data in that block can be of any type
+output sample_list_op" {
+    value   =   "Welcome to ${var.sample_list[0]} ${var.sample_list[1]}  and duration is ${var.sample_list[2]} hours"
+}
