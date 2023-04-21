d3fd64420a4510f83b0c05e60047ac7be8042084
Pramod kumar chinthala <pramodkumar.chinthala@gmail.com>
2023-04-21 11:53:09 +0200

    Auto push

--- terraform_basics/03-example/ec_2/main.tf
+++ terraform_basics/03-example/ec_2/main.tf
@@ -7,6 +7,20 @@ resource "aws_instance" "web" {
     tags = {
         Name = "Terraform-instance-Name"
     }
+    # Declaring the remote provisioner inside the resource
+    provisioner "remote-exec" {
+
+        connection {                   // establishes connectivity to the created machine
+            type     = "ssh"
+            user     = "centos"
+            password = "DevOps321"
+            host     = self.private_ip   
+      }
+
+    inline = [
+        "ansible-pull -U https://github.com/Pramod07Ch/DevOps.git ansible/robot-pull.yml -e ENV=dev -e COMPONENT=mongodb"
+    ]
+  }
 }
 
 variable "sg" {}
